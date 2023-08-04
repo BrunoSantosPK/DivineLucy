10973081a6e87cfc076e6daf760a5d3383b8755c
BrunoSantosPK <bruno.ajsch@yahoo.com>
2023-08-04 08:55:24 -0300

    rotas para criacao, edicao e delecao

--- src/controllers/record.py
+++ src/controllers/record.py
@@ -1,7 +1,7 @@
 import json
 from flask import request
 from src.utils.transfer import Transfer
-from src.services.record import RecordService
+from src.services.record import RecordService, RecordDetails
 
 
 class RecordController:
@@ -14,6 +14,11 @@ class RecordController:
             # Verifica JWT enviado
             user_id = request.view_args["user_id"]
             page = int(request.args.get("page", default=1))
+            res = RecordService.get_all(user_id, page=page)
+            if not res.success:
+                raise Exception(res.message)
+            
+            integration.set_data(res.records)
 
         except BaseException as e:
             integration.set_message(str(e))
@@ -28,7 +33,30 @@ class RecordController:
 
         try:
             # Verifica JWT enviado
+            # Avalia campos opcionais
             body = json.loads(request.data)
+            if "details" not in body.keys():
+                body["details"] = []
+            if "origin_id" not in body.keys():
+                body["origin_id"] = None
+
+            # Transforma lista de detalhes
+            for i in range(0, len(body["details"])):
+                body["details"][i] = RecordDetails(body["details"][i]["description"], body["details"][i]["value"])
+
+            res = RecordService.new(
+                body["user_id"],
+                body["item_id"],
+                body["target_id"],
+                body["moviment_date"],
+                body["description"],
+                body["value"],
+                origin_id=body["origin_id"],
+                details=body["details"]
+            )
+            if not res.success:
+                raise Exception(res.message)
+            integration.set_data({"record_id": res.record_id})
 
         except BaseException as e:
             integration.set_message(str(e))
@@ -43,7 +71,31 @@ class RecordController:
 
         try:
             # Verifica JWT enviado
+            # Avalia campos opcionais
             body = json.loads(request.data)
+            if "details" not in body.keys():
+                body["details"] = []
+            if "origin_id" not in body.keys():
+                body["origin_id"] = None
+
+            # Transforma lista de detalhes
+            for i in range(0, len(body["details"])):
+                body["details"][i] = RecordDetails(body["details"][i]["description"], body["details"][i]["value"])
+
+            # Faz a atualização
+            res = RecordService.edit(
+                body["record_id"],
+                body["user_id"],
+                body["item_id"],
+                body["target_id"],
+                body["moviment_date"],
+                body["description"],
+                body["value"],
+                origin_id=body["origin_id"],
+                details=body["details"]
+            )
+            if not res.success:
+                raise Exception(res.message)
 
         except BaseException as e:
             integration.set_message(str(e))
@@ -59,6 +111,9 @@ class RecordController:
         try:
             # Verifica JWT enviado
             body = json.loads(request.data)
+            res = RecordService.delete(body["record_id"], body["user_id"])
+            if not res.success:
+                raise Exception(res.message)
 
         except BaseException as e:
             integration.set_message(str(e))
--- src/tests/test_record.py
+++ src/tests/test_record.py
@@ -15,7 +15,8 @@ track_test_data = {
     "item_id": "fb0b8338-63ab-4ccf-8ebf-8e4b2798b08a",
     "target_id": "24d3bbbc-20a2-4350-bf96-408501af4a4b",
     "origin_id": "7961fd02-ac54-458a-a9ae-e3d57241aa70",
-    "record_id_service": ""
+    "record_id_service": "",
+    "record_id_controller": ""
 }
 
 
@@ -125,3 +126,107 @@ def test_service_delete_record_with_origin():
     res = RecordService.delete(track_test_data["record_id_service"], track_test_data["user_id"])
     assert res.message == ""
     assert res.success
+
+
+def test_controller_new_record_without_origin_and_with_details(client: FlaskClient):
+    body = {
+        "user_id": track_test_data["user_id"],
+        "item_id": track_test_data["item_id"],
+        "target_id": track_test_data["target_id"],
+        "moviment_date": "2023-08-03",
+        "description": "Movimentação de teste",
+        "value": 456.69,
+        "details": [
+            {"description": "Item A", "value": 56.69},
+            {"description": "Item B", "value": 400}
+        ]
+    }
+    res = client.post("/record", json=body)
+    data = json.loads(res.get_data(as_text=True))
+    track_test_data["record_id_controller"] = data["data"]["record_id"]
+
+    assert data["message"] == ""
+    assert data["status_code"] == 200
+    assert data["data"]["record_id"] != ""
+
+
+def test_controller_edit_record_remove_details_and_add_origin(client: FlaskClient):
+    body = {
+        "record_id": track_test_data["record_id_controller"],
+        "user_id": track_test_data["user_id"],
+        "item_id": track_test_data["item_id"],
+        "target_id": track_test_data["target_id"],
+        "moviment_date": "2023-08-03",
+        "description": "Movimentação de teste",
+        "origin_id": track_test_data["origin_id"],
+        "value": 300,
+    }
+    res = client.put("/record", json=body)
+    data = json.loads(res.get_data(as_text=True))
+
+    assert data["message"] == ""
+    assert data["status_code"] == 200
+
+
+def test_controller_delete_record_with_details(client: FlaskClient):
+    body = {
+        "user_id": track_test_data["user_id"],
+        "record_id": track_test_data["record_id_controller"]
+    }
+    res = client.delete("/record", json=body)
+    data = json.loads(res.get_data(as_text=True))
+
+    assert data["message"] == ""
+    assert data["status_code"] == 200
+
+
+def test_controller_new_record_with_origin_and_without_details(client: FlaskClient):
+    body = {
+        "user_id": track_test_data["user_id"],
+        "item_id": track_test_data["item_id"],
+        "target_id": track_test_data["target_id"],
+        "moviment_date": "2023-08-03",
+        "description": "Movimentação de teste",
+        "value": 456.69,
+        "origin_id": track_test_data["origin_id"]
+    }
+    res = client.post("/record", json=body)
+    data = json.loads(res.get_data(as_text=True))
+    track_test_data["record_id_controller"] = data["data"]["record_id"]
+
+    assert data["message"] == ""
+    assert data["status_code"] == 200
+    assert data["data"]["record_id"] != ""
+
+
+def test_controller_edit_record_add_details_remove_origin(client: FlaskClient):
+    body = {
+        "record_id": track_test_data["record_id_controller"],
+        "user_id": track_test_data["user_id"],
+        "item_id": track_test_data["item_id"],
+        "target_id": track_test_data["target_id"],
+        "moviment_date": "2023-08-03",
+        "description": "Movimentação de teste",
+        "value": 300,
+        "details": [
+            {"description": "Item A", "value": 56.69},
+            {"description": "Item B", "value": 400}
+        ]
+    }
+    res = client.put("/record", json=body)
+    data = json.loads(res.get_data(as_text=True))
+
+    assert data["message"] == ""
+    assert data["status_code"] == 200
+
+
+def test_controller_delete_record_witout_details(client: FlaskClient):
+    body = {
+        "user_id": track_test_data["user_id"],
+        "record_id": track_test_data["record_id_controller"]
+    }
+    res = client.delete("/record", json=body)
+    data = json.loads(res.get_data(as_text=True))
+
+    assert data["message"] == ""
+    assert data["status_code"] == 200
